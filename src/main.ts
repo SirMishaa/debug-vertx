23260de989fc3d501026d9e6c764d6335bd1a7ef
mishaa <djoly@optimy.com>
2022-06-21 15:13:12 +0200

    :bug: remove process.env

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,16 @@ import './style.css'
 const app = document.querySelector<HTMLDivElement>('#app')!
 console.log(app.children.length)
 
-const ID_TOKEN_KEY = 'id_token'
-
 export function login () {
 
     const auth = new auth0.WebAuth({
-        clientID: ID_TOKEN_KEY,
-        domain: "test.fr"
+        clientID: 'test',
+        domain: 'test.fr'
     })
 
     auth.authorize({
-        responseType: 'token id_token',
-        redirectUri: "process.env.VUE_APP_AUTH0_CALLBACK_URL"
+        responseType: 'token',
+        redirectUri: 'callback'
     })
 }
 
